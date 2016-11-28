dff3b7f0d0d711b49d7b533a24741efdcc8d95cc
Tako12 <boroda9000@ya.ru>
2016-11-29 00:23:45 +0300

    В задании со звездочкой скоращена запись

--- Homework_4.js
+++ Homework_4.js
@@ -38,14 +38,15 @@ function countChar (string, char) {
 // Задание со звездочкой
 
 function isEven (N) {
-  if (N<0) {
+	if (N<0) {
     N = -N; // Это исправление
   }
-  if (N === 0) {
-   return true;
- } else if (N === 1) {
-    return false;
-  } else { 
-    return isEven (N-2);
-  }
+	if (N == 0 || N == 1) {
+		return  !N;
+	} else {
+	  return isEven (N-2);
+ 		}
 }
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-1));
